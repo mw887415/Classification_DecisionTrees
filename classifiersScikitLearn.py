50b9a45f9fa1c35d40768e2b87dfe8d57a598241
Wang Michel <45081916+mw887415@users.noreply.github.com>
2021-04-29 18:01:54 +0200

    Add files via upload

---
+++ classifiersScikitLearn.py
@@ -0,0 +1,21 @@
+from sklearn import tree
+
+clf_decision_tree = tree.DecisionTreeClassifier()
+
+# [height, weight, shoe_size]
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
+     [190, 90, 47], [175, 64, 39],
+     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
+     'female', 'male', 'male']
+
+
+#and train them on our data
+clf_decision_tree = clf_decision_tree.fit(X, Y)
+
+prediction =   clf_decision_tree.predict([[190, 70, 43]])
+prediction_2 =  clf_decision_tree.predict([[153,55,39]])
+
+print(prediction)
+print(prediction_2)

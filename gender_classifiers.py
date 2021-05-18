243b670553e0a5cc9c47c38ded707ca297a3eaf6
Michel Wang <michel.wang@rwth-aachen.de>
2021-05-19 00:41:43 +0200

    Added comments to the different steps

--- gender_classifiers.py
+++ gender_classifiers.py
@@ -31,22 +31,27 @@ clf_KNN.fit(X, Y)
 # Testing using the same data
 pred_tree = clf_tree.predict(X)
 acc_tree = accuracy_score(Y, pred_tree) * 100
+#Result: 100
 print('Accuracy for DecisionTree: {}'.format(acc_tree))
 
 pred_svm = clf_svm.predict(X)
 acc_svm = accuracy_score(Y, pred_svm) * 100
+#Result: 54,54...
 print('Accuracy for SVM: {}'.format(acc_svm))
 
 pred_per = clf_perceptron.predict(X)
 acc_per = accuracy_score(Y, pred_per) * 100
+#Result: 45,45...
 print('Accuracy for perceptron: {}'.format(acc_per))
 
 pred_KNN = clf_KNN.predict(X)
 acc_KNN = accuracy_score(Y, pred_KNN) * 100
+#Result: 72,72...
 print('Accuracy for KNN: {}'.format(acc_KNN))
 
 # The best classifier from svm, per, KNN
-index = np.argmax([acc_svm, acc_per, acc_KNN])
+# We left out the DecisionTreeClassifier since there is high likelihood of
+# overfitting, which in practice translates into a bad classifier
+index = np.argmax([acc_svm, acc_per, acc_KNN]) # Returns the indices of the maximum values along an axis.
 classifiers = {0: 'SVM', 1: 'Perceptron', 2: 'KNN'}
 print('Best gender classifier is {}'.format(classifiers[index]))
-
